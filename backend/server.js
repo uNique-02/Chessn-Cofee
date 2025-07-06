5f175fb0ee21e3c1ebc319155ee97d41c160cda3
kimz <92148017+uNique-02@users.noreply.github.com>
2025-07-06 23:51:05 +0800

    Debugging deployment problem

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,9 @@ if (process.env.NODE_ENV === "production") {
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-    app.get("/*", (req, res) => {
-      res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-    });
+    // app.get("/*", (req, res) => {
+    //   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    // });
   }
 }
 app.listen(PORT, () => {
